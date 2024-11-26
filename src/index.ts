e72a31149906150ccec78a11082ae8360a907130
Rene Malmgren <rene.malmgren@h3.se>
2024-11-26 19:51:34 +0400

    Update project for tsc

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './org/nostr/ses/Subscription'
+export * from './org/nostr/ses/Publisher'
+export * from './org/nostr/ses/SynchronisedSession'
+export * from './org/nostr/ses/SynchronisedEventStream'
+
